2067853d104503d4e99834b24e9a48839c3a1357
Yashasvi1015 <yashasvisharma846@gmail.com>
2024-06-12 15:51:47 +0530

    Create challenge6.sol
    Write a Solidity contract with 4 functions; add, subtract, multiply and divide 2 numbers.

---
+++ challenge6.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract ArithmeticOperations {
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+
+    // Function to subtract the second number from the first number
+    function subtract(uint256 a, uint256 b) public pure returns (uint256) {
+        return a - b;//a>b
+    }
+    function multiply(uint256 a, uint256 b) public pure returns (uint256) {
+        return a * b;
+    }
+    function divide(uint256 a, uint256 b) public pure returns (uint256) {
+        return a / b;
+    }
+}
